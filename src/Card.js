84d5ba3e1ed703aa75caa0c797430a33e9f4b9a7
Nishant Deshpande <deshpande.nishant12@gmail.com>
2024-05-05 12:42:31 +0530

    Fetched additional information for movie titles

--- src/Card.js
+++ src/Card.js
@@ -1,18 +1,55 @@
 import { Link } from "react-router-dom";
 import "./Card.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Card = ({ movie }) => {
+    const TMDB_FETCH_LINK_PREPEND = "https://api.themoviedb.org/3/movie/";
+
     const [movieIMDBID, setMovieIMDBID] = useState(movie["imdbID"]);
+    const [movieBudget, setMovieBudget] = useState(0);
+    const [movieOverview, setMovieOverview] = useState("");
+    const [movieRuntime, setMovieRuntime] = useState(0);
+    // const [movieGenres, setMovieGenres] = useState([]);
+    // var movieBudget, movieOverview, movieRuntime;
 
     if (!movie["imdbID"]) {
         setMovieIMDBID("default");
     }
 
+    var additionalDetailsFetchURL = TMDB_FETCH_LINK_PREPEND + movieIMDBID;
+
+    const getAdditionalMovieDetails = () => {
+        const options = {
+            method: "GET",
+            headers: {
+                accept: "application/json",
+                Authorization:
+                    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MjZlNGIxYjc0YWY3ODg3MzkwZGQ2ZWI4MjAxYWM1MCIsInN1YiI6IjY2MjNhOWE4ODdlNjNlMDE4ODczNjI4NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.g41xhry5OzBREjLQN65hVt2qZ53hnxNJMZZ-GT0IQGQ",
+            },
+        };
+
+        if (movieIMDBID != "default") {
+            fetch(additionalDetailsFetchURL, options)
+                .then((response) => response.json())
+                .then((response) => {
+                    console.log(response);
+                    setMovieBudget(response["budget"]);
+                    setMovieOverview(response["overview"]);
+                    setMovieRuntime(response["runtime"]);
+                    // setMovieGenres(...response["genres"]);
+                })
+                .catch((err) => console.error(err));
+        }
+    };
+
     var movieTitle = movie["Title"];
     var movieYear = movie["Year"];
     var moviePoster = movie["Poster"];
     var titleType = movie["Type"];
+
+    useEffect(() => {
+        getAdditionalMovieDetails();
+    }, []);
 
     return (
         <div>
@@ -38,8 +75,15 @@ const Card = ({ movie }) => {
                         }}
                     >
                         <div className="card-back-content">
-                            <div className="imdb-id">
-                                IMDB ID: {movieIMDBID}
+                            <div className="additional-details">
+                                <h4 className="card-back-subheading">IMDB ID</h4>
+                                <p className="card-back-details">{movieIMDBID}</p>
+                                <h4 className="card-back-subheading">Budget</h4>
+                                <p className="card-back-details">{movieBudget}</p>
+                                <h4 className="card-back-subheading">Runtime</h4>
+                                <p className="card-back-details">{movieRuntime} minutes</p>
+                                <h4 className="card-back-subheading">Overview</h4>
+                                <p className="card-back-details details-overview">{movieOverview}</p>
                             </div>
                             <div className="link-to-view">
                                 <button className="button-to-view">
